1722e8faddde681404221c73a4a606977776e812
Oleksandr Vernyhora <lamarcus@ukr.net>
2018-10-08 18:19:35 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
 
+const el = (
+    <div>
+        <h1>Todo List</h1>
+        <input placeholder="search" />
+        <ul>
+            <li>One</li>
+            <li>Two</li>
+            <li>Three</li>
+        </ul>
+    </div>
+)
+
+ReactDOM.render(el, document.getElementById("root"));
