e6cd25559d4c30f84f563312bda1e8ab4c712d5d
Oleksandr Vernyhora <lamarcus@ukr.net>
2018-10-22 10:12:37 +0300

    added state to the components

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -3,6 +3,7 @@ import React from "react";
 import TodoListItem from "../todo-list-item/todo-list-item";
 import "./todo-list.css";
 
+
 const TodoList = ({ todos }) => {
   const elements = todos.map(item => {
     const { id, ...itemProps } = item;
