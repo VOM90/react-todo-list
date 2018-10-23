c5aa6972520d06b33dcdbd9281ceb02e60d75562
Oleksandr Vernyhora <lamarcus@ukr.net>
2018-10-23 11:20:56 +0300

    added imput for task adding

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -2,16 +2,36 @@ import React, { Component } from "react";
 import "./item-add-form.css";
 
 export default class ItemAddForm extends Component {
+  state = {
+    label: ""
+  };
+
+  onLabelChange = e => {
+    this.setState({
+      label: e.target.value
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onItemAdded(this.state.label);
+  };
+
   render() {
     return (
-      <div className="item-add-form">
+      <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          className="form-control"
+          onChange={this.onLabelChange}
+          placeholder="Put your task here..."
+        />
         <button
-          className="btn btn-outline-secondary"
-          onClick={() => this.props.onItemAdded("Hello world")}
+          className="btn btn-outline-secondary"         
         >
           Add Item
         </button>
-      </div>
+      </form>
     );
   }
 }
